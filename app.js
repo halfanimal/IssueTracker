038ab9c34bd04a13e4735cef4e0e4b16180e0688
Raphael Ungricht <raphaelungricht@gmx.ch>
2016-12-02 15:58:41 +0100

    added api for projects

--- app.js
+++ app.js
@@ -14,6 +14,25 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.delete('/api/project/:project_id/issues/:id', function(req, res){
+    console.log(req.params.project_id);
+    res.send(JSON.stringify(req.params.project_id));
+});
+
+
+app.put('/api/project/:project_id/issues/:id', function(req, res){
+    console.log(req.params.project_id);
+    res.send(JSON.stringify(req.params.project_id));
+});
+
+app.get('/api/project/:project_id/issues', function(req, res) {
+
+});
+
+app.post('/api/project/:project_id/issues', function(req, res) {
+
+});
+
 app.listen(port, function () {
     console.log('Server listening on: http://localhost:%s', port);
 });
