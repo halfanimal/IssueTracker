022b4e1c0f26ab51dc56485b4e4df8fa9e454486
Furkan Arslan <arslafur@students.zhaw.ch>
2016-12-03 11:26:52 +0100

    Finished RESTful API

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 "use strict";
-
+var fs = require('fs');
+var path = require("path");
 var express = require('express');
-const path = require("path");
+var body_parser = require('body-parser');
 
 var app = express();
-var fs = require('fs');
+var port = parseInt(process.env.PORT, 10) || 8080;;
+var dbFilePath =  __dirname + "/" + "database.json";
 
-const port = 8080;
-const dbFilePath =  __dirname + "/" + "database.json";
-
+app.use(body_parser.json());
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
@@ -32,17 +32,36 @@ app.delete('/api/project/:project_id/issues/:id', function(req, res){
         fs.writeFile(dbFilePath, JSON.stringify(data), 'utf8', function (err) {
             if (err)
                 return console.log(err);
-        });
 
-        res.json(data);
+            res.json(data);
+        });
         //res.send('Issue ' + req.params.id + ' from project ' + req.params.project_id + ' deleted');
     });
 });
 
 
 app.put('/api/project/:project_id/issues/:id', function(req, res){
-    console.log(req.params.project_id);
-    res.json(req.params.project_id);
+    console.log('Updating issue: ' + req.params.id);
+
+    fs.readFile(dbFilePath, 'utf8', function (err, data) {
+        if (err)
+           console.log(err);
+
+        data = JSON.parse(data);
+
+        var reqIssue = createIssueObj(req.body);
+
+        data['issues'] = data['issues'].map(function(issue) {
+            return (issue.project_id == req.params.project_id && issue.id == req.params.id) ? reqIssue : issue;
+        });
+
+        fs.writeFile(dbFilePath, JSON.stringify(data), 'utf8', function(err, data) {
+            if(err)
+                console.log(err);
+        });
+
+        res.json(reqIssue);
+    });
 });
 
 app.get('/api/project/:project_id/issues', function(req, res) {
@@ -61,31 +80,85 @@ app.get('/api/project/:project_id/issues', function(req, res) {
 });
 
 app.post('/api/project/:project_id/issues', function(req, res) {
+    console.log('Create and save issue');
 
+    fs.readFile(dbFilePath, 'utf8', function (err, data) {
+        if (err)
+            console.log(err);
+
+        data = JSON.parse(data);
+
+        var reqIssue = createIssueObj(req.body);
+
+        data['issues'].push(reqIssue);
+
+        fs.writeFile(dbFilePath, JSON.stringify(data), 'utf8', function(err, data) {
+            if(err)
+                console.log(err);
+
+            res.json(reqIssue);
+        });
+    });
 });
 
 app.get('/api/tests/plus', function(req, res) {
-
+    res.send('Not implemented yet');
 });
 
 app.post('/api/tests/minus', function(req, res) {
-
+    res.send('Not implemented yet');
 });
 
 app.post('/api/tests/echo', function(req, res) {
-
+    res.send('Not implemented yet');
 });
 
 app.put('/api/tests/echo', function(req, res) {
-
+    res.send('Not implemented yet');
 });
 
 app.post('/api/projects', function(req, res) {
+    console.log('Create and save project');
 
+    fs.readFile(dbFilePath, 'utf8', function (err, data) {
+        if (err)
+            console.log(err);
+
+        data = JSON.parse(data);
+
+        var reqProject = createProjectObj(req.body);
+
+        data['projects'].push(reqProject);
+
+        fs.writeFile(dbFilePath, JSON.stringify(data), 'utf8', function(err, data) {
+            if(err)
+                console.log(err);
+
+            res.json(reqProject);
+        });
+    });
 });
 
 app.delete('/api/projects/:id', function(req, res) {
+    console.log('Delete project ' + req.params.id);
 
+    fs.readFile(dbFilePath, 'utf8', function (err, data) {
+        if(err)
+            return console.log(err);
+
+        data = JSON.parse(data);
+
+        data['projects'] = data['projects'].filter(function (project) {
+            return project.id != req.params.id;
+        });
+
+        fs.writeFile(dbFilePath, JSON.stringify(data), 'utf8', function (err) {
+            if (err)
+                return console.log(err);
+
+            res.json(data);
+        });
+    });
 });
 
 app.get('/api/projects/:id', function(req, res) {
@@ -97,7 +170,7 @@ app.get('/api/projects/:id', function(req, res) {
 
         data = JSON.parse(data);
 
-        res.json(data['projects'].filter(function (project) {
+        res.json(data['projects'].find(function (project) {
            return project.id == req.params.id;
         }));
     });
@@ -120,18 +193,50 @@ app.get('/api/projects/', function(req, res) {
 });
 
 app.put('/api/projects/:id', function(req, res) {
-    console.log('Put project: ' + req.params.id);
+    console.log('Updating project: ' + req.params.id);
 
-    var project = new ProjectModel({
-        id: req.body.id,
-        client_id: req.body.client_id,
-        title: req.body.title,
-        active: req.body.active
+    fs.readFile(dbFilePath, 'utf8', function (err, data) {
+        if (err)
+            console.log(err);
+
+        data = JSON.parse(data);
+
+        var reqProject = createProjectObj(req.body);
+
+        data['projects'] = data['projects'].map(function(project) {
+            return (project.id == req.params.id) ? reqProject : project;
+        });
+
+        fs.writeFile(dbFilePath, JSON.stringify(data), 'utf8', function(err, data) {
+            if(err)
+                console.log(err);
+        });
+
+        res.json(data);
     });
-
-    
 });
 
 app.listen(port, function () {
     console.log('Server listening on: http://localhost:%s', port);
 });
+
+function createIssueObj(reqBody) {
+    return {
+        id: reqBody.id,
+        project_id: reqBody.project_id,
+        client_id: reqBody.client_id,
+        project_client_id: reqBody.project_client_id,
+        done: reqBody.done,
+        title: reqBody.title,
+        priority: reqBody.priority,
+        due_date: reqBody.due_date
+    };
+}
+
+function createProjectObj(reqBody) {
+    return {id: reqBody.id,
+        client_id: reqBody.client_id,
+        title: reqBody.title,
+        active: reqBody.active
+    };
+}
